4eb7a8933f7baf3e91cf309c3f3fe0b6ff00735b
AviduDev <avidudev@gmail.com>
2023-09-02 17:56:56 +0530

    getting starting

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+
+
+export default function Hero() {
+    return(
+        <div className="flex flex-col">
+            <h1>find any car at any time with ease!</h1>
+        </div>
+    )
+}
